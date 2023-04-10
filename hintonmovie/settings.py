3475632db4bf744c5d7ffb02fce753cd00c3cfc8
dinhngocthuuyen <thuuyen.0621@gmail.com>
2023-04-11 00:40:44 +0700

    update cors

--- hintonmovie/settings.py
+++ hintonmovie/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
