ad8795f94ba0f92ef3945926b42bf5304155020a
dinhngocthuuyen <thuuyen.0621@gmail.com>
2023-04-10 21:40:46 +0700

    update media

--- hintonmovie/settings.py
+++ hintonmovie/settings.py
@@ -136,8 +136,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # media files configuration
-MEDIA_ROOT = 'https://stephenkiendinh.com/media/'
-MEDIA_URL = 'https://stephenkiendinh.com/media/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
