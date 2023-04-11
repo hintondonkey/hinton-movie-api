c065471642ceaa8d8c06d701bf7fb7276fcafc27
dinhngocthuuyen <thuuyen.0621@gmail.com>
2023-04-11 23:58:34 +0700

    update cors

--- hintonmovie/settings.py
+++ hintonmovie/settings.py
@@ -171,7 +171,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0:5500/",
     "http://localhost:21690/",
     "https://stephenkiendinh.com/",
-    "http://localhost:53144/"
+    "http://localhost:53501/",
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
