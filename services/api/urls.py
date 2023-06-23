43f22df4bd2bf90908059be4cd0333f7849d1aaf
Thanh La <63770606+lvthanhpn@users.noreply.github.com>
2023-06-23 21:23:18 +0700

    update services

--- services/api/urls.py
+++ services/api/urls.py
@@ -4,5 +4,7 @@ from ..api import views
 
 
 urlpatterns = [
-    
+    path("get_sub_category/", views.SubCategoryListAPIView.as_view(), name="get_subcategory_list"),
+    path("sub_category/", views.SubCategoryAPIView.as_view(), name="list_create_subcategory_event"),
+    path("sub_category/<int:pk>/", views.SubCategoryRetrieveUpdateDestroyAPIView.as_view(), name="get_update_delete_subcategory_event"),
 ]
