74f6f168ca359683aabad4bd17e9a08674219600
Thanh La <63770606+lvthanhpn@users.noreply.github.com>
2023-06-30 14:55:50 +0700

    Update Category

--- services/api/views.py
+++ services/api/views.py
@@ -194,7 +194,7 @@ class SubCategoryCategoryBrokerListAPIView(ListAPIView):
     serializer_class = SubCategorySerializer
 
     def get_queryset(self):
-        category_id_list = BrokerService.objects.filter(broker_id=self.kwargs['broker_id'], is_active=True).values_list('category_id', flat=True)
+        category_id_list = BrokerService.objects.filter(broker_id=self.kwargs['broker_id'], category_id=self.kwargs['category_id'], is_active=True).values_list('category_id', flat=True)
         return SubCategory.objects.filter(category_id__in=category_id_list, broker_id=self.kwargs['broker_id']).distinct('id')
     
      
